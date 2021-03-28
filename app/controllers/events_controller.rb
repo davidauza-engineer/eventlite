75ef6a6adf24e3f06ea2f1224c3aa1115b4f957a
davidauza.engineer <davidauza01@gmail.com>
2021-03-27 20:33:23 -0500

    Create Event model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,27 +2,6 @@
 
 class EventsController < ApplicationController
   def index
-    @events = [
-      {
-        title: 'London Retail Expo',
-        start_datetime: 'Mon, 21 Oct 2019',
-        location: 'London Excel Centre'
-      },
-      {
-        title: 'Enterprise Sales Training Workshop',
-        start_datetime: 'Tue, 22 Oct 2019',
-        location: 'Expert Sales Company Headquarters'
-      },
-      {
-        title: 'Ruby Hack Night',
-        start_datetime: 'Fri, 25 Oct 2019',
-        location: 'Learnetto Headquarters'
-      },
-      {
-        title: 'Beginners Salsa dance meetup',
-        start_datetime: 'Sat, 26 Oct 2019',
-        location: 'Bar Salsa'
-      }
-    ]
+    @events = Event.order('start_datetime ASC')
   end
 end
