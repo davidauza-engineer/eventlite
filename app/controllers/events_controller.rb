ef6c71ca5858d448ad2d1e1966563b8bdb85c04c
davidauza.engineer <davidauza01@gmail.com>
2021-03-27 19:19:32 -0500

    Create Eventlite component

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
-  def index; end
+  def index
+    @events = [
+      {
+        title: 'London Retail Expo',
+        start_datetime: 'Mon, 21 Oct 2019',
+        location: 'London Excel Centre'
+      },
+      {
+        title: 'Enterprise Sales Training Workshop',
+        start_datetime: 'Tue, 22 Oct 2019',
+        location: 'Expert Sales Company Headquarters'
+      },
+      {
+        title: 'Ruby Hack Night',
+        start_datetime: 'Fri, 25 Oct 2019',
+        location: 'Learnetto Headquarters'
+      },
+      {
+        title: 'Beginners Salsa dance meetup',
+        start_datetime: 'Sat, 26 Oct 2019',
+        location: 'Bar Salsa'
+      }
+    ]
+  end
 end
